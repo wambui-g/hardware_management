a3ab3f1273845b62bddd0ab2e74f95769f408487
wambui-g <grayciekamau03@gmail.com>
2024-04-19 12:56:56 +0300

    issues with inventory+invoice

--- project/invoice/models.py
+++ project/invoice/models.py
@@ -1,1 +1,28 @@
 from django.db import models
+from inventory.models import Subcategory, Item
+
+class Invoice(models.Model):
+    invoice_number = models.CharField(max_length=100)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    count = models.PositiveIntegerField(default=0)
+    invoiced_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    FULFILLMENT_CHOICES = (
+        ('PAID', 'Paid'),
+        ('UNPAID', 'Unpaid'),
+    ) 
+    fulfillment = models.CharField(max_length=100, choices=FULFILLMENT_CHOICES)
+    date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if self.fulfillment == "PAID":
+            # Update inventory count if invoice is paid
+            item = self.item
+            item.count -= self.count
+            item.save()
+        self.invoiced_amount = self.price * self.count
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.invoice_number
--- project/invoice/views.py
+++ project/invoice/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Invoice
+
 
 def invoice(request):
-    template = loader.get_template("invoice.html")
-    return HttpResponse(template.render())
+    # Retrieve all invoices from the database
+    invoices = Invoice.objects.all()
+
+    # Pass the invoices to the template context
+    context = {'invoices': invoices}
+    
+    # Render the invoice template with the invoices data
+    return render(request, 'invoice.html', context)
